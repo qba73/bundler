9531154373f551f4bcd078f7b7ec505d4f361d21
Jakub Jarosz <jakub@jarosz.dev>
2023-03-27 19:33:30 +0100

    Update repo structure

--- bundler.go
+++ bundler.go
@@ -1,5 +1,11 @@
 package bundler
 
+import "fmt"
+
 func Main() int {
 	return 1
+}
+
+func RunServer() {
+	fmt.Println("running dummy server...")
 }
---
+++ cmd/bundler-api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/qba73/bundler"
+
+func main() {
+	bundler.RunServer()
+}
